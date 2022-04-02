4176db93f5bdf5005b4543a170af95cc8d60ad34
KevinDGnanih <kevin.gnanih@gmail.com>
2022-04-02 13:59:14 +0000

    latest version

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,18 +1,18 @@
 function sendMail(contactForm) {
     emailjs.send("Gmail", "m&m", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            document.getElementById(reply).innertext = `<p>RSVP submitted with success! Thank you</p>`;
-        },
-        function(error) {
-            console.log("FAILED", error);
-            document.getElementById(reply).innertext = `<p>Please fill up all the sections.</p>`;
-        });
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "message": contactForm.message.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                alert("Thank you!");
+            },
+            function (error) {
+                console.log("FAILED", error);
+                document.getElementById(reply).innertext = `<p>Please fill up all the sections.</p>`;
+            });
 
     return false;
 }
